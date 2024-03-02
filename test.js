f2a2a7e62efd0bc4dd8fe17f3e25b111f233e095
DiasKabdualiyev <dias.31407@gmail.com>
2024-03-02 19:55:31 +0600

    asdda

--- test.js
+++ test.js
@@ -15,7 +15,10 @@ let tcpSocket = null; // Для хранения сокета TCP-клиента
 const tcpServer = net.createServer((socket) => {
     console.log(`Ридер подключен с адреса: ${socket.remoteAddress}`);
     tcpSocket = socket; // Сохраняем сокет для последующего использования
-
+    const initialCommand = Buffer.from([0x55, 0xAA, 0x55, 0x00, 0x25, 0x80, 0x03, 0xA8]);
+    socket.write(initialCommand, () => {
+        console.log('Инициализирующая команда отправлена клиенту');
+    });
     socket.on('data', (data) => {
         console.log(`Получены данные: ${data.toString('hex')}`);
         //Полученный пример данных FF 00 06 83 02 02 B3 7B 06 C1 [Tag Serial:0x 02 B3 7B 06][UID Length:4][Tag Type:0x02]
@@ -38,9 +41,20 @@ tcpServer.listen(TCP_PORT, TCP_HOST, () => {
 // Настройка HTTP-сервера на Express для обработки GET-запросов
 httpApp.get('/open-door', (req, res) => {
     if (tcpSocket) {
-        const command = Buffer.from([0xFF, 0x00, 0x08, 0xD0, 0x00, 0x02, 0x00, 0x64, 0x00, 0x64, 0x00, 0xA2]);
+        const command = Buffer.from([0xFF, 0x00, 0x08, 0xD0, 0x09, 0x02, 0x00, 0x32, 0x00, 0x32, 0x00, 0x47]);
         tcpSocket.write(command, () => {
             res.send('Команда "Activate All" отправлена');
+        });
+    } else {
+        res.status(404).send('TCP клиент не подключен');
+    }
+});
+
+httpApp.get('/reset', (req, res) => {
+    if (tcpSocket) {
+        const command = Buffer.from([0xFF, 0x00, 0x01, 0x80, 0x81]);
+        tcpSocket.write(command, () => {
+            res.send('Команда "Reset" отправлена');
         });
     } else {
         res.status(404).send('TCP клиент не подключен');
